8b9f7a367445f0249f00fbb1d3c1296217b68162
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-12-05 13:24:25 +0600

    added review details and highest rated game functionality

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const port = process.env.PORT || 5010
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5010;
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
-
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iofbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -34,19 +29,38 @@ async function run() {
     const database = client.db("gameDB");
     const reviewCollection = database.collection("reviews");
 
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post('/reviews', async(req,res)=>{
-    const newReview = req.body; 
-    console.log('New Review Item:', newReview);
-    const result = await reviewCollection.insertOne(newReview); 
-    res.send(result);
-    })
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.get("/highest-rated-reviews", async (req, res) => {
+      const cursor = reviewCollection.find().sort({ rating: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const newReview = req.body;
+      console.log("New Review Item:", newReview);
+      const result = await reviewCollection.insertOne(newReview);
+      res.send(result);
+    });
 
 
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -54,13 +68,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("GameWhiz SERVER IS RUNNING");
+});
 
-
-
-app.get('/',(req,res)=>{
-    res.send('GameWhiz SERVER IS RUNNING')
-})
-
-app.listen(port,()=>{
-    console.log(`GameWhiz SERVER IS RUNNING ON PORT : ${port}`)
-})
+app.listen(port, () => {
+  console.log(`GameWhiz SERVER IS RUNNING ON PORT : ${port}`);
+});
